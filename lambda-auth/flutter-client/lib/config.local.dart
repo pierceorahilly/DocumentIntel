3c617846821ba0b457dff4dc27e84bbf78b75a20
pierceorahilly <orahillypierce@gmail.com>
2026-06-04 02:01:09 +0100

    feat: add branch-specific API configuration

---
+++ lambda-auth/flutter-client/lib/config.local.dart
@@ -0,0 +1,8 @@
+// Local development config - DO NOT COMMIT
+// Uncomment the environment you want to use
+
+// Staging environment
+const String API_BASE_URL = 'https://vz1307i8t2.execute-api.eu-west-1.amazonaws.com/staging';
+
+// Production environment (uncomment to use)
+// const String API_BASE_URL = 'https://vz1307i8t2.execute-api.eu-west-1.amazonaws.com/prod';
--- lambda-auth/flutter-client/lib/core/config.dart
+++ lambda-auth/flutter-client/lib/core/config.dart
@@ -1,6 +1,7 @@
+import '../config.local.dart' as localConfig;
+
 class AppConfig {
-  static const String apiBaseUrl =
-      'https://vz1307i8t2.execute-api.eu-west-1.amazonaws.com/prod';
+  static const String apiBaseUrl = localConfig.API_BASE_URL;
   static const String cognitoUserPoolId = 'eu-west-1_BSofFOCGg';
   static const String cognitoClientId = '1aiq9l331h0locean5mnut4nlk';
   static const String awsRegion = 'eu-west-1';
