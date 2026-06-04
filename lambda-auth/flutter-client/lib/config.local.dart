f630b0984ed122d5f4e3bd6074d888c514c53f9c
pierceorahilly <orahillypierce@gmail.com>
2026-06-04 02:10:40 +0100

    config: point main to production API

---
+++ lambda-auth/flutter-client/lib/config.local.dart
@@ -0,0 +1,8 @@
+// Local development config - DO NOT COMMIT
+// Uncomment the environment you want to use
+
+// Staging environment (comment out)
+// const String API_BASE_URL = 'https://vz1307i8t2.execute-api.eu-west-1.amazonaws.com/staging';
+
+// Production environment
+const String API_BASE_URL = 'https://vz1307i8t2.execute-api.eu-west-1.amazonaws.com/prod';
